06961b230ab48055393275ca7be0a6682390b5ad
chuan-wxy <3153917921@qq.com>
2025-01-16 13:02:37 +0800

    新增状态码

--- woj-common/src/main/java/com/chuan/wojcommon/common/ResultStatus.java
+++ woj-common/src/main/java/com/chuan/wojcommon/common/ResultStatus.java
@@ -9,16 +9,29 @@ package com.chuan.wojcommon.common;
 
 public enum ResultStatus {
 
-    SUCCESS(200,"成功"),
-
-    FAIL(400,"失败"),
-
-    UNAUTHORIZED(401,"未登录或登录过期，请重新登录"),
-
-    FORBIDDEN(403,"拒绝访问"),
-
-    NOT_FOUND(404,"数据不存在"),
-
+    OK(200,"OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202,  "Accepted"),
+    NON_AUTHORITATIVE_INFORMATION(203, "Non-Authoritative Information"),
+    NO_CONTENT(204,  "No Content"),
+    RESET_CONTENT(205,  "Reset Content"),
+    PARTIAL_CONTENT(206,  "Partial Content"),
+    MULTI_STATUS(207,  "Multi-Status"),
+    ALREADY_REPORTED(208,  "Already Reported"),
+    IM_USED(226, "IM Used"),
+    MULTIPLE_CHOICES(300,  "Multiple Choices"),
+    MOVED_PERMANENTLY(301,  "Moved Permanently"),
+    FOUND(302,  "Found"),
+    TEMPORARY_REDIRECT(307,  "Temporary Redirect"),
+    PERMANENT_REDIRECT(308,  "Permanent Redirect"),
+    FAIL(400,  "FAIL"),
+    UNAUTHORIZED(401,  "Unauthorized"),
+    PAYMENT_REQUIRED(402,  "Payment Required"),
+    FORBIDDEN(403,  "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    NOT_ACCEPTABLE(406, "Not Acceptable"),
+    PROXY_AUTHENTICATION_REQUIRED(407,  "Proxy Authentication Required"),
     SYSTEM_ERROR(500,"系统错误");
 
     private int code;
